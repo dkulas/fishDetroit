9e324e407bc9984f3156dd42066306d40ba39044
dkulas <danny.kulas1@gmail.com>
2024-08-21 12:49:22 -0400

    updated dartsass.rb to include variables

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -1,5 +1,6 @@
 # config/initializers/dartsass.rb
 Rails.application.config.dartsass.builds = {
-  "common/common.scss" => "common.css",
-  "config/reset.scss" => "reset.css"
+  "config/reset.scss" => "reset.css",
+  "config/variables.scss" => "variables.css",
+  "common/common.scss" => "common.css"
 }
