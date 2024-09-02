9655a9ccee6b7a7223b6ff3dd9a5e55052c5d7eb
dkulas <danny.kulas1@gmail.com>
2024-09-02 16:28:58 -0400

    updated gitignore and styles, added dartsass.rb file

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -1,6 +1,4 @@
 # config/initializers/dartsass.rb
 Rails.application.config.dartsass.builds = {
-  "config/reset.scss" => "reset.css",
-  "config/variables.scss" => "variables.css",
-  "common/common.scss" => "common.css"
+  "." => "."
 }
