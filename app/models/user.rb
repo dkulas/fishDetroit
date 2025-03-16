628f06331732249a29fac6cfb72388527f06a346
dkulas <danny.kulas1@gmail.com>
2025-03-16 16:27:08 -0400

    updated user model email uniqueness requirements

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true
   
 	has_many :posts, dependent: :destroy
